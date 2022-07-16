81181d3cc5396da491f158b0d63178881b7bd4eb
Sellig6792 <sellig6792@gmail.com>
2022-07-16 17:15:36 +0200

    Add a function system to BrainFuck

--- interpreter/__init__.py
+++ interpreter/__init__.py
@@ -7,7 +7,11 @@ class Interpreter:
 
                  stack: list = None,
                  cells: list[int] = None,
+                 function_cells: list[str] = None,
+
+                 is_function: bool = False,
                  ):
+        self.is_function = is_function
         self.i = 0
         self.code = code.replace('\n', '').replace('\t', '').replace(' ', '')
         if ',' in self.code and not stdin:
@@ -15,6 +19,7 @@ class Interpreter:
 
         self.stack = stack or []
         self.cells = cells or [0] * 30000
+        self.function_cells = function_cells or [''] * 30000
 
         self.stdin = stdin
         self.stdout = stdout
@@ -22,6 +27,7 @@ class Interpreter:
         self.pointer = 0
 
         self.in_comment: bool = False
+        self.writing_function: bool = False
 
     @property
     def cell(self):
@@ -31,7 +37,11 @@ class Interpreter:
     def cell(self, value):
         self.cells[self.pointer] = value
 
-    def __call__(self, *args, **kwargs):
+    @property
+    def function_cell(self):
+        return self.function_cells[self.pointer]
+
+    def __call__(self, *args, **kwargs) -> tuple[str, str]:
         while self.i < len(self.code):
             instruction = self.code[self.i]
 
@@ -40,6 +50,13 @@ class Interpreter:
                 self.in_comment = not self.in_comment
                 continue
             if self.in_comment:
+                self.i += 1
+                continue
+
+            if instruction == ':':
+                self.function()
+            elif self.writing_function:
+                self.function_cells[self.pointer] += instruction
                 self.i += 1
                 continue
 
@@ -62,9 +79,21 @@ class Interpreter:
                 self.start_loop()
             elif instruction == ']':
                 self.end_loop()
+
+            elif instruction == 'x':
+                self.execute()
             self.i += 1
 
-        print(self.stdout)
+        if self.is_function:
+            return self.stdin, self.stdout
+        else:
+            print(self.stdout)
+
+    def function(self):
+        """
+        Write the code between the matching : and : to the function cell at the data pointer.
+        """
+        self.writing_function = not self.writing_function
 
     def right(self):
         """
@@ -128,3 +157,16 @@ class Interpreter:
         jump it back to the command after the matching [ command.
         """
         self.i = self.stack.pop() - 1
+
+    def execute(self):
+        """
+        Execute the function at the data pointer.
+        """
+        function = Interpreter(
+            self.function_cell,
+            self.stdin, self.stdout,
+            self.stack, self.cells,
+            self.function_cells, is_function=True
+        )
+        self.stdin, self.stdout = function()
+
