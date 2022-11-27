f80688c7b13b573197c24a62e339203fc75a9458
Sellig6792 <sellig6792@gmail.com>
2022-11-27 11:07:01 +0100

    Classic Brainfuck interpreter

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+mod ast;
+mod evaluator;
+
+use std::fs;
+
+use evaluator::Evaluator;
+
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+
+    // Get path and quit if it's not provided
+    let path = match args.get(1) {
+        Some(path) => path,
+        None => {
+            println!("Please provide a path to a file");
+            return;
+        }
+    };
+
+
+    // Read the file and quit if it's not found
+    let program = if fs::metadata(&path).is_ok() {
+        fs::read_to_string(&path).expect("Unable to read file")
+    } else {
+        println!("File not found");
+        return;
+    };
+
+    // Parse the program
+    let mut parser = ast::Parser::new(program);
+    let instructions = parser.parse();
+
+    // Run the program
+    let mut brainfuck = Evaluator::new(instructions);
+    brainfuck.evaluate(None, None);
+}
