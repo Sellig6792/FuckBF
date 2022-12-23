595a849a13f27c4e6651825c13933ec4007cf643
Sellig6792 <sellig6792@gmail.com>
2022-12-23 19:52:53 +0100

    Add CLI

--- src/main.rs
+++ src/main.rs
@@ -4,37 +4,46 @@ mod optimization;
 
 use std::fs;
 
+use anyhow::{Context, Result};
+use clap::Parser;
+
 use crate::evaluation::Evaluator;
 
-fn main() {
-    let args: Vec<String> = std::env::args().collect();
+#[derive(Parser)]
+#[command(author, version, about)]
+struct CLI {
+    // Path is positional argument
+    #[arg(required = true, help = "Path to the file to execute")]
+    path: std::path::PathBuf,
 
-    // Get path and quit if it's not provided
-    let path = match args.get(1) {
-        Some(path) => path,
-        None => {
-            println!("Please provide a path to a file");
-            return;
-        }
-    };
+    #[arg(short = 'O', long = "optimize", help = "Optimize the code", default_value = "false")]
+    optimize: bool,
+}
 
+
+fn main() ->  Result<(), Box<dyn std::error::Error>>{
+    let args= CLI::parse();
     // Read the file and quit if it's not found
-    let program = if fs::metadata(&path).is_ok() {
-        fs::read_to_string(&path).expect("Unable to read file")
-    } else {
-        println!("File not found");
-        return;
-    };
+    let program = fs::read_to_string(&args.path)
+        .with_context(|| format!("Could not read file `{}`", args.path.to_str().unwrap()))?;
 
     // Parse the program
     let mut parser = ast::Parser::new(program);
     let instructions = parser.parse();
 
-    // Optimize the program
-    let mut optimizer = optimization::Optimizer::new(instructions.clone());
-    let optimized_instructions = optimizer.optimize();
+    // Optimize the program if "optimize" is true
+    if args.optimize {
+        let mut optimizer = optimization::Optimizer::new(instructions.clone());
+        let optimized_instructions = optimizer.optimize();
 
-    // Run the program
-    let mut brainfuck = Evaluator::new(optimized_instructions);
-    brainfuck.evaluate(None, None);
+        // Evaluate the optimized program
+        let mut brainfuck = Evaluator::new(optimized_instructions);
+        brainfuck.evaluate(None, None);
+    } else {
+        // Evaluate the program
+        let mut brainfuck = Evaluator::new(instructions);
+        brainfuck.evaluate(None, None);
+    }
+
+    Ok(())
 }
