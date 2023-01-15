c6ef86933cc4a8408d2a808a6c60b7e3bf0c5754
Sellig6792 <sellig6792@gmail.com>
2023-01-15 19:25:57 +0100

    Remove update system to move it to the installer project

--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,9 @@ mod fuckbf;
 use cli::Cli;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // Before running help or version, delete the old file
-    cli::update::delete_old_file();
-
     let args = Cli::parse();
 
-    if args.update {
-        cli::update::update()?;
-    } else if let Some(path) = args.path {
+    if let Some(path) = args.path {
         cli::run(&path, args.optimize)?;
     } else {
         println!("{} The following required arguments were not provided:\n  {}\n\n{} {} [OPTIONS] [PATH]\n\nFor more information try {}",
